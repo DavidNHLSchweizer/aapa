72d36bd4929aaa5ca328f824ad7b234a8cba1027
David Schweizer <david.schweizer@nhlstenden.com>
2023-01-13 15:52:28 +0100

    cleanup

--- nuitka-build.bat
+++ nuitka-build.bat
@@ -1,7 +1,7 @@
 @set starttime=%DATE% %TIME%
 @echo Started: %starttime%
 
-python -m nuitka --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy --noinclude-unittest-mode=allow --windows-icon-from-ico=aapa.ico aapa.py 
+python -m nuitka --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy --noinclude-unittest-mode=allow --windows-icon-from-ico=aapa_icon.ico aapa.py 
 :: unittest-mode is nodig omdat dat gebruikt (?misbruikt) wordt in fpdf voor iets vaags met signing pdfs (wat ik dus niet doe) maar het kan er dus ook niet zomaar uit 
 
 @set endtime=%DATE% %TIME%
