b065fce54d4847748115dadfdd841e3417f8a8f4
David Schweizer <david.schweizer@nhlstenden.com>
2023-01-13 15:47:08 +0100

    changes for nuitka building

---
+++ nuitka-build.bat
@@ -0,0 +1,10 @@
+@set starttime=%DATE% %TIME%
+@echo Started: %starttime%
+
+python -m nuitka --standalone --onefile --enable-plugin=tk-inter --enable-plugin=numpy --noinclude-unittest-mode=allow --windows-icon-from-ico=aapa.ico aapa.py 
+:: unittest-mode is nodig omdat dat gebruikt (?misbruikt) wordt in fpdf voor iets vaags met signing pdfs (wat ik dus niet doe) maar het kan er dus ook niet zomaar uit 
+
+@set endtime=%DATE% %TIME%
+@echo Ready: %endtime%
+@python batchtimer.py "%starttime%" "%endtime%" "total build time: "
+
