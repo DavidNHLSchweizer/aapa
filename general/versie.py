9f720de8fdaa540bad067406eb0db06ebb439c8b
David Schweizer <david.schweizer@nhlstenden.com>
2023-01-28 15:45:42 +0100

    added kopieer-functie aanvraag naar forms-directory

--- general/versie.py
+++ general/versie.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 import datetime
 from general.config import config
 
-AAPAVERSION = '0.97'
+AAPAVERSION = '0.98'
 @dataclass
 class Versie:
     versie: str = config.get('versie', 'versie')
