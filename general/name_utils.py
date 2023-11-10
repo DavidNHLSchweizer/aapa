e8b603ecd311d38d3e074365003165336529c0be
David Schweizer <david.schweizer@nhlstenden.com>
2023-11-10 11:06:25 +0100

    name utils finalized including unittest

--- general/name_utils.py
+++ general/name_utils.py
@@ -1,53 +1,84 @@
-from general.config import ListValueConvertor, config
+from __future__ import annotations
+from dataclasses import dataclass
+import re
+from general.config import config
+from general.singleton import Singleton
 
-def init_config():
-    config.register('names', 'tussen', ListValueConvertor)
-    config.init('names', 'tussen', ['van', 'de', 'der', 'den', 'te', 'in'])
-init_config()
+@dataclass
+class ParsedName:
+    first_name:str=''
+    tussen:str=''
+    last_name:str=''    
+    def serialize(self)->str:
+        return f'{self.first_name}|{self.tussen}|{self.last_name}'
+    @staticmethod
+    def deserialize(value: str)->ParsedName:
+        words = value.split('|')
+        return ParsedName(first_name=words[0], tussen=words[1], last_name=words[2])
+    def __eq__(self,p2: ParsedName)->bool:
+        return self.first_name==p2.first_name and self.tussen==p2.tussen and self.last_name==p2.last_name
+
+class SpecialCases(Singleton):
+    def __init__(self):
+        self._cases = {}
+    def add(self, full_name: str, parsed: ParsedName):
+        self._cases[full_name.lower()] = parsed
+    def contains(self, full_name: str)->bool:
+        return full_name.lower() in self._cases
+    def parsed_name(self, full_name: str)->ParsedName:
+        return self._cases.get(full_name.lower(), None)
+    def load_from_config(self):
+        try:            
+            for special_name,value in config.items('special_names'):
+                self.add(special_name, ParsedName.deserialize(value))
+        except:
+            pass
+    def save_to_config(self):
+        for special_name, parsed in self._cases.items():
+            config.set('special_names', special_name, parsed.serialize())
+special_cases = SpecialCases()
+
+def init_special_cases():
+    special_cases.load_from_config()
+    special_cases.add('Rob Klein Ikink', ParsedName('Rob', '', 'Klein Ikink'))
+    special_cases.save_to_config()
+init_special_cases()
 
 class Names:
     @staticmethod
-    def tussenvoegsels()->list[str]:
-        return config.get('names', 'tussen')
+    def parsed(full_name: str)->ParsedName:
+        TUSSEN_PATTERN = r"(?P<tussen>\b(der|den|de|van|ter|te|in\'t|in\s't|in)\b)+?"
+        if special_cases.contains(full_name):
+            return special_cases.parsed_name(full_name)
+        first_tussen_start = -1
+        last_tussen_end = -1        
+        tussens = []
+        for match in re.finditer(TUSSEN_PATTERN, full_name):
+            tussen_start = match.start('tussen')
+            if tussen_start > last_tussen_end and full_name[last_tussen_end:tussen_start].strip(): # catch extreme case like "van Voorst de Water"
+                break
+            if first_tussen_start == -1:
+                first_tussen_start = tussen_start
+            tussens.append(match.group('tussen'))
+            last_tussen_end = match.end('tussen')
+        if tussens:
+            return ParsedName(first_name=full_name[0:first_tussen_start].strip(), tussen=' '.join(tussens), last_name=full_name[last_tussen_end:].strip())
+        else:
+            words = full_name.split(' ')
+            return ParsedName(first_name=' '.join(words[:len(words)-1]), last_name=words[len(words)-1])
     @staticmethod
     def first_name(full_name: str)->str:
-        if full_name and (words := full_name.split(' ')):
-            return words[0]
-        return ''
+        return Names.parsed(full_name).first_name
     @staticmethod
-    def tussen(full_name: str, first_name='')->str:
-        voegsels=Names.tussenvoegsels()
-        words = full_name.split(' ')
-        start_last_name = Names.__get_nr_first_name_words(full_name, first_name)
-        tussen_words = []
-        while start_last_name < len(words) and words[start_last_name] in voegsels:
-            tussen_words.append(words[start_last_name])
-            start_last_name+= 1
-        return ' '.join(tussen_words)
+    def tussen(full_name: str)->str:
+        return Names.parsed(full_name).tussen
     @staticmethod
-    def __get_nr_first_name_words(full_name: str, first_name='')->int:
-        words = full_name.split(' ')
-        first = words[0]
-        voegsels = Names.tussenvoegsels()
-        result = 1
-        while result < len(words) and len(first) < len(first_name):
-            if words[result] in voegsels:
-                break
-            first  = first + ' ' + words[result]
-            result += 1
-        if result < len(words):
-            return result
-        return 1
-    @staticmethod
-    def last_name(full_name: str, first_name='', include_tussen: bool=True):
-        if full_name and (words := full_name.split(' ')):
-            start = Names.__get_nr_first_name_words(full_name,first_name)
-            voegsels = Names.tussenvoegsels()
-            if not include_tussen: 
-                while words[start] in voegsels and start < len(words)-1:
-                    start +=1
-            return ' '.join(words[start:len(words)])
-        return ''   
+    def last_name(full_name: str, include_tussen: bool=True):
+        parsed = Names.parsed(full_name)        
+        if include_tussen:
+            return f'{parsed.tussen} {parsed.last_name}'
+        else:
+            return parsed.last_name
     @staticmethod
     def initials(full_name: str='', email: str = '')->str:
         result = ''
@@ -58,5 +89,3 @@ class Names:
             for word in full_name.split(' '):
                 result += word[0]
         return result.lower()
-
-
--- tests/general/test_names.py
+++ tests/general/test_names.py
@@ -1,42 +1,5 @@
-from __future__ import annotations
-from dataclasses import dataclass
-import re
-# from general.name_utils import Names
+from general.name_utils import Names, ParsedName, special_cases
 
-
-special_cases = {'Jan Pieter Klaassen Katrijns':'Jan Pieter||Klaassen Katrijns',
-                 'Jan-Pieter Klaassen Katrijns':'Jan-Pieter||Klaassen Katrijns'}
-
-
-
-@dataclass
-class ParsedName:
-    first_name:str=''
-    tussen:str=''
-    last_name:str=''
-    def __eq__(self,p2: ParsedName)->bool:
-        return self.first_name==p2.first_name and self.tussen==p2.tussen and self.last_name==p2.last_name
-
-class Names:
-    @staticmethod
-    def parsed(full_name: str)->ParsedName:
-        TUSSEN_PATTERN = r"(?P<tussen>\b(der|den|de|van|ter|te|in\'t|in\s't|in)\b){1,2}?"
-        if special_case := special_cases.get(full_name, None):
-            words=special_case.split('|')
-            print(words)
-            return ParsedName(first_name=words[0], tussen=words[1], last_name=words[2])
-        match = re.findall(TUSSEN_PATTERN, full_name)
-        if match:
-            tussen = [m for (m,_) in match]
-            # print(tussen)
-            # for (m1,_) in match:
-            #     print(m1)
-            first_part_start = full_name.find(tussen[0])            
-            last_part_end = full_name.find(tussen[len(tussen)-1]) + len(tussen[len(tussen)-1])
-            return ParsedName(first_name=full_name[0:first_part_start].strip(), tussen=' '.join(tussen), last_name=full_name[last_part_end:].strip())
-        else:
-            words = full_name.split(' ')
-            return ParsedName(first_name=' '.join(words[:len(words)-1]), last_name=words[len(words)-1])
 testcases1 = [ 
     {'name': '',  'parsed': ParsedName('', '', '')},
     {'name': 'Jan',  'parsed': ParsedName('', '', 'Jan')},
@@ -69,7 +32,6 @@ testcases4 = [
     {'name': "Jan Pieter in 't Klaassen",  'parsed': ParsedName('Jan Pieter', "in 't", 'Klaassen')},]
 
 testcases5 = [ 
-    {'name': 'Jan Pieter Klaassen Katrijns',  'parsed': ParsedName('Jan Pieter', '', 'Klaassen Katrijns')},
     {'name': 'Jan Pieter van Klaassen Katrijns',  'parsed': ParsedName('Jan Pieter', 'van', 'Klaassen Katrijns')},
     {'name': 'Jan Pieter van de Klaassen Katrijns',  'parsed': ParsedName('Jan Pieter', 'van de', 'Klaassen Katrijns')},
     {'name': 'Jan Pieter van der Klaassen Katrijns',  'parsed': ParsedName('Jan Pieter', 'van der', 'Klaassen Katrijns')},
@@ -78,7 +40,6 @@ testcases5 = [
     {'name': "Jan Pieter in 't Klaassen Katrijns",  'parsed': ParsedName('Jan Pieter', "in 't", 'Klaassen Katrijns')},]
 
 testcases6 = [ 
-    {'name': 'Jan-Pieter Klaassen Katrijns',  'parsed': ParsedName('Jan-Pieter', '', 'Klaassen Katrijns')},
     {'name': 'Jan-Pieter van Klaassen Katrijns',  'parsed': ParsedName('Jan-Pieter', 'van', 'Klaassen Katrijns')},
     {'name': 'Jan-Pieter van de Klaassen Katrijns',  'parsed': ParsedName('Jan-Pieter', 'van de', 'Klaassen Katrijns')},
     {'name': 'Jan-Pieter van der Klaassen Katrijns',  'parsed': ParsedName('Jan-Pieter', 'van der', 'Klaassen Katrijns')},
@@ -92,11 +53,17 @@ testcases7 = [
     {'name': 'Jan van der Klaassin',  'parsed': ParsedName('Jan', 'van der', 'Klaassin')},
     {'name': 'Jan de Klaassede',  'parsed': ParsedName('Jan', 'de', 'Klaassede')},
     {'name': 'Jan ter Klaassen',  'parsed': ParsedName('Jan', 'ter', 'Klaassen')},
-    {'name': "Jan in 't Klaassen",  'parsed': ParsedName('Jan', "in 't", 'Klaassen')},]
+    {'name': "Jan in 't Klaassen",  'parsed': ParsedName('Jan', "in 't", 'Klaassen')},
+    {'name': "Jan-Pieter van Voorst de Tering", 'parsed': ParsedName('Jan-Pieter', "van", 'Voorst de Tering')},
+    ]
 
+testcases_special = [ 
+    {'name': 'Jan Pieter Klaassen Katrijns',  'parsed': ParsedName('Jan Pieter', '', 'Klaassen Katrijns')},
+    {'name': 'Jan-Pieter Klaassen Katrijns',  'parsed': ParsedName('Jan-Pieter', '', 'Klaassen Katrijns')},
+    ]
 
 def _test(testcases):
-    for testcase in testcases:
+    for testcase in testcases:        
         assert Names.parsed(testcase['name']) == testcase['parsed']
 
 def test_testcase_1():
@@ -119,3 +86,8 @@ def test_testcase_6():
 
 def test_testcase_7():
     _test(testcases7)        
+
+def test_testcase_special():
+    for entry in testcases_special:
+        special_cases.add(entry['name'],entry['parsed'])
+    _test(testcases_special)        
