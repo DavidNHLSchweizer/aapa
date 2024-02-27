a208e14ef87e25705750ce263bd30139dc43479b
David Schweizer <david.schweizer@nhlstenden.com>
2024-02-27 09:49:39 +0100

    reconciliatie

---
+++ m125.bat
@@ -0,0 +1,98 @@
+@echo off
+set onedrive=d:\onedrive
+set basedir="%onedrive%\NHL Stenden\HBO-ICT Afstuderen - Software Engineering\2023-2024 Nieuw\aapa"
+set db0=%basedir%\aapa_124.db
+set db1=%basedir%\aapa.db
+set mxx=migrate.m125
+if not exist %db0% copy /y %db1% %db0% 
+set v0=1.24
+set v1=1.25
+
+set phase1=%1
+set phase2=%2
+if "%2"=="" set phase2=%phase1%
+set msg=Phases %phase1% to %phase2%
+if "%1"=="" (
+   set phase1=0
+   set phase2=42
+   set msg=All phases
+)
+@echo AAPA MIGRATION SCRIPT %v0% to %v1%. %msg%
+goto :phase%phase1%
+
+:phase0
+set phase=0
+call :migrate %phase%
+goto :phase42:
+call :msgnext
+call :plugin %mxx%.ccc
+if "%phase2%" LSS "1" goto :phase42
+
+:phase1
+set phase=1
+call :migrate %phase%
+call :msgnext
+call :plugin %mxx%.ttt
+if "%phase2%" LSS "2" goto :phase42
+
+:phase2
+set phase=2
+call :migrate %phase%
+call :msgnext
+call :plugin %mxx%.rrr	
+if "%phase2%" LSS "3" goto :phase42
+
+:phase3
+set phase=3
+call :migrate %phase%
+goto :phase42:
+call :msgnext
+call :plugin %mxx%.ttt  %mxx%.ttt
+if "%phase2%" LSS "4" goto :phase42
+
+:phase4
+set phase=4
+call :migrate %phase%
+call :msgnext
+call :plugin %mxx%.zzz
+if "%phase2%" LSS "5" goto :phase42
+
+:phase5
+set phase=5
+call :migrate %phase%
+call :msgnext
+call :plugin %mxx%.xxx
+if "%phase2%" LSS "6" goto :phase42
+
+:phase6
+set phase=6
+call :migrate %phase%
+goto :phase42
+
+:plugin
+python run_plugin.py %~1 %~2 %~3 %~4 %~5 --onedrive=%onedrive% -json -debug
+exit /b
+
+:reset
+copy /y %db0% %db1% >NUL
+exit /b
+
+:migrate
+echo ---------------------------
+echo --- MIGRATION PHASE %~1 ---
+echo ---------------------------
+call :reset
+python aapa_migrate.py %db1% %v0% %v1% --phase=%~1 -debug
+exit /b
+
+:msgnext
+set /a next=phase+1
+echo ---------------------------------------------
+echo --- preparing JSON files for phase %next% ---
+echo ---------------------------------------------
+exit /b
+
+:phase42
+set phase1=
+set phase2=
+
---
+++ migrate/migrate_124_125.py
@@ -0,0 +1,98 @@
+""" migratie naar database v1.25
+
+Aanpassingen database (Aggregation CRUD).
+
+"""
+from dataclasses import dataclass
+from enum import Enum, auto
+from data.classes.aanvragen import Aanvraag
+from data.classes.files import File
+from data.classes.mijlpaal_directories import MijlpaalDirectory
+from data.classes.verslagen import Verslag
+from data.general.class_codes import ClassCodes
+from database.aapa_database import AanvraagDetailsTableDefinition, AanvragenFileOverzichtDefinition,AanvragenFileOverzichtDefinition, MijlpaalDirectoryDetailsTableDefinition,StudentDirectoryDetailsTableDefinition, StudentDirectoriesFileOverzichtDefinition,StudentMijlpaalDirectoriesOverzichtDefinition,StudentVerslagenOverzichtDefinition,   UndologDetailsTableDefinition, VerslagDetailsTableDefinition
+from database.classes.sql_table import SQLcreateTable, SQLdropTable
+from database.classes.sql_view import SQLcreateView, SQLdropView
+from migrate.m124.obsolete import AanvraagFilesTableDefinition, MijlpaalDirectory_FilesTableDefinition, StudentDirectory_DirectoriesTableDefinition, UndoLogAanvragenTableDefinition, UndoLogFilesTableDefinition, UndoLogVerslagenTableDefinition, VerslagFilesTableDefinition, oldAanvragenFileOverzichtDefinition, oldStudentDirectoriesFileOverzichtDefinition, oldStudentMijlpaalDirectoriesOverzichtDefinition, oldStudentVerslagenOverzichtDefinition
+from migrate.migrate import JsonData
+from database.classes.database import Database
+ 
+class M125JsonData(JsonData):
+    class KEY(Enum):
+        CORRECT_VERSLAGEN_DOUBLURES = auto()
+    def __init__(self):
+        super().__init__(r'migrate\m125')
+        self.init_entries()
+    def init_entries(self):
+        # self.add_entry(self.KEY.CORRECT_VERSLAGEN_DOUBLURES,filename='correct_verslagen_doublures', phase=3, message ='correcting doublure verslagen')
+        pass
+
+def drop_views(database: Database):
+    database.execute_sql_command(SQLdropView(oldAanvragenFileOverzichtDefinition()))
+    database.execute_sql_command(SQLdropView(oldStudentDirectoriesFileOverzichtDefinition()))
+    database.execute_sql_command(SQLdropView(oldStudentVerslagenOverzichtDefinition()))
+    database.execute_sql_command(SQLdropView(oldStudentMijlpaalDirectoriesOverzichtDefinition()))
+    database.execute_sql_command(SQLdropView(AanvragenFileOverzichtDefinition()))
+    database.execute_sql_command(SQLdropView(StudentDirectoriesFileOverzichtDefinition()))
+    database.execute_sql_command(SQLdropView(StudentVerslagenOverzichtDefinition()))
+    database.execute_sql_command(SQLdropView(StudentMijlpaalDirectoriesOverzichtDefinition()))
+def create_aanvragen(database: Database):
+    database.execute_sql_command(SQLdropTable(AanvraagDetailsTableDefinition()))
+    database.execute_sql_command(SQLcreateTable(AanvraagDetailsTableDefinition()))
+    database._execute_sql_command(
+        f'INSERT into AANVRAGEN_DETAILS(aanvraag_id,detail_id,class_code) select aanvraag_id,file_id,"{ClassCodes.classtype_to_code(File)}" from AANVRAGEN_FILES')
+def create_mijlpaal_directories(database: Database):
+    database.execute_sql_command(SQLdropTable(MijlpaalDirectoryDetailsTableDefinition()))
+    database.execute_sql_command(SQLcreateTable(MijlpaalDirectoryDetailsTableDefinition()))
+    database._execute_sql_command(
+        f'INSERT into MIJLPAAL_DIRECTORIES_DETAILS(mp_dir_id,detail_id,class_code) select mp_dir_id,file_id,"{ClassCodes.classtype_to_code(File)}" from MIJLPAAL_DIRECTORY_FILES')     
+def create_student_directories(database: Database):
+    database.execute_sql_command(SQLdropTable(StudentDirectoryDetailsTableDefinition()))
+    database.execute_sql_command(SQLcreateTable(StudentDirectoryDetailsTableDefinition()))
+    database._execute_sql_command(
+        f'INSERT into STUDENT_DIRECTORIES_DETAILS(stud_dir_id,detail_id,class_code) select stud_dir_id,mp_dir_id,"{ClassCodes.classtype_to_code(MijlpaalDirectory)}" from STUDENT_DIRECTORY_DIRECTORIES')
+def create_undologs(database: Database):
+    database.execute_sql_command(SQLdropTable(UndologDetailsTableDefinition()))
+    database.execute_sql_command(SQLcreateTable(UndologDetailsTableDefinition()))
+    database._execute_sql_command(
+        f'INSERT into UNDOLOGS_DETAILS(log_id,detail_id,class_code) select log_id,aanvraag_id,"{ClassCodes.classtype_to_code(Aanvraag)}" from UNDOLOGS_AANVRAGEN')
+    database._execute_sql_command(
+        f'INSERT into UNDOLOGS_DETAILS(log_id,detail_id,class_code) select log_id,file_id,"{ClassCodes.classtype_to_code(File)}" from UNDOLOGS_FILES')
+    database._execute_sql_command(
+        f'INSERT into UNDOLOGS_DETAILS(log_id,detail_id,class_code) select log_id,verslag_id,"{ClassCodes.classtype_to_code(Verslag)}" from UNDOLOGS_VERSLAGEN')
+def create_verslagen(database: Database):
+    database.execute_sql_command(SQLdropTable(VerslagDetailsTableDefinition()))
+    database.execute_sql_command(SQLcreateTable(VerslagDetailsTableDefinition()))
+    database._execute_sql_command(
+        f'INSERT into VERSLAGEN_DETAILS(verslag_id,detail_id,class_code) select verslag_id,file_id,"{ClassCodes.classtype_to_code(File)}" from VERSLAGEN_FILES')
+def drop_old_tables(database: Database):
+    database.execute_sql_command(SQLdropTable(UndoLogAanvragenTableDefinition()))
+    database.execute_sql_command(SQLdropTable(UndoLogVerslagenTableDefinition()))
+    database.execute_sql_command(SQLdropTable(UndoLogFilesTableDefinition()))
+    database.execute_sql_command(SQLdropTable(AanvraagFilesTableDefinition()))
+    database.execute_sql_command(SQLdropTable(VerslagFilesTableDefinition()))
+    database.execute_sql_command(SQLdropTable(AanvraagFilesTableDefinition()))
+    database.execute_sql_command(SQLdropTable(MijlpaalDirectory_FilesTableDefinition()))
+    database.execute_sql_command(SQLdropTable(StudentDirectory_DirectoriesTableDefinition()))    
+def create_views(database: Database):
+    database.execute_sql_command(SQLcreateView(AanvragenFileOverzichtDefinition()))
+    database.execute_sql_command(SQLcreateView(StudentDirectoriesFileOverzichtDefinition()))
+    database.execute_sql_command(SQLcreateView(StudentVerslagenOverzichtDefinition()))
+    database.execute_sql_command(SQLcreateView(StudentMijlpaalDirectoriesOverzichtDefinition()))
+
+def migrate_database(database: Database, phase = 42):    
+    with database.pause_foreign_keys():
+        drop_views(database)
+        create_aanvragen(database)
+        create_mijlpaal_directories(database)
+        create_student_directories(database)
+        create_undologs(database)
+        create_verslagen(database)
+        drop_old_tables(database)
+        create_views(database)
+        M125JsonData().execute(database, phase)
+
+def after_migrate(database_name: str, debug=False, phase=42):
+    pass # just testing. To be done later if necessary. Get a clearer way to (re)produce the SQL scripts.
+        
+         # 
